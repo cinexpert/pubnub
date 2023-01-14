3e96bddf688112eb27f06b2e0679dcb7cf72e873
Pascal Paulis <ppaulis@gmail.com>
2023-01-14 22:23:16 +0100

    working on pubnub setup
    Signed-off-by: Pascal Paulis <ppaulis@gmail.com>

--- src/PubNub/PubNubService.php
+++ src/PubNub/PubNubService.php
@@ -51,7 +51,7 @@ class PubNubService
             $pnConfiguration
                 ->setPublishKey($this->publisherKey)
                 ->setSubscribeKey($this->subscriberKey)
-                ->setUserId($this->userId)
+                ->setUuid($this->userId)
                 ->setSecure(true);
 
             $this->pubNubClient = new PubNub($pnConfiguration);
